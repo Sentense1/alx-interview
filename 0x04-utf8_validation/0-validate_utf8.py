471137aba0d058d037682efbe21bb50e79ead263
sentense1 <3xsaint@gmail.com>
2023-10-23 18:13:03 +0000

    modified the script for accurate output

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,44 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/python3
 """
-Module for utf-8 validation
+Module to validate UTF-8 encoding
 """
 from typing import List
 
 
 def validUTF8(data: List[int]) -> bool:
     """
-    Validates data for utf-8
+    Validates whether the given data represents valid UTF-8 encoding.
+
+    Args:
+        data (List[int]): List of integers, each representing 1 byte of data.
+
+    Returns:
+        bool: True if data is valid UTF-8 encoding, False otherwise.
     """
-    # Initialize a variable to keep track of the number of continuation bytes
     num_continuation_bytes = 0
 
-    # Iterate through the list of integers (each representing 1 byte of data).
     for byte in data:
-        # Check if the byte is a continuation byte (starts with '10' in binary)
+        # Ensure each byte has 8 bits in binary representation
+        binary_byte = bin(byte)[2:].rjust(8, '0')
+
         if num_continuation_bytes > 0:
-            if bin(byte)[2:4] != '10':
+            if not binary_byte.startswith('10'):
                 return False
             num_continuation_bytes -= 1
         else:
-            # Determine the number of continuation bytes for the current char
-            if bin(byte)[2:4] == '11':
-                num_continuation_bytes = 1
-            elif bin(byte)[2:5] == '111':
-                num_continuation_bytes = 2
-            elif bin(byte)[2:6] == '1111':
-                num_continuation_bytes = 3
-            elif bin(byte)[2] == '0':
+            if binary_byte.startswith('0'):
                 num_continuation_bytes = 0
+            elif binary_byte.startswith('110'):
+                num_continuation_bytes = 1
+            elif binary_byte.startswith('1110'):
+                num_continuation_bytes = 2
+            elif binary_byte.startswith('11110'):
+                num_continuation_bytes = 3
             else:
                 return False
 
-    # If there are unmatched continuation bytes, return False.
     return num_continuation_bytes == 0
+
 
 if __name__ == "__main__":
     data = [65]
